6643c1ee00a7065cc9d3d06c08273d0d2d8f4f12
Luis Angel Anampa Lavado <u202218664@upc.edu.pe>
2024-06-24 23:31:03 -0500

    elmaspro

--- src/main/java/unirider/platform/CarpoolingPlatformApplication.java
+++ src/main/java/unirider/platform/CarpoolingPlatformApplication.java
@@ -2,6 +2,10 @@ package unirider.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -9,8 +13,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class CarpoolingPlatformApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(CarpoolingPlatformApplication.class, args);
     }
 
+    @Configuration
+    public static class MyConfiguration {
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
+    }
 }
